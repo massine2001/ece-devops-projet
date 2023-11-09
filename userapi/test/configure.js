f476c5d8a051ae3f68d5a951012b215f146284e6
Agharmiou Massine <agharmioumassine@gmail.com>
2023-11-09 18:11:49 +0100

    fix: fix classe de connexion

--- userapi/test/configure.js
+++ userapi/test/configure.js
@@ -4,11 +4,11 @@ const configure = require('../src/configure')
 describe('Configure', () => {
   it('load default json configuration file', () => {
     const config = configure()
-    expect(config.redis).to.eql({"host": "127.0.0.1", "port": 6379})
+    expect(config.redis).to.eql({"host": "ece-devops-projet.redis.cache.windows.net", "port": 6379, "password":"mslEyUxY5x9pj9HPL6wRzjglqFhr2KyGNAzCaFYNpw8="})
   })
   it('load custom configuration', () => {
     const config_custom = {"custom": "value"}
     const config = configure(config_custom)
-    expect(config).to.eql({"redis": {"host": "127.0.0.1", "port": 6379}, "custom": "value"})
+    expect(config).to.eql({"redis": {"host": "ece-devops-projet.redis.cache.windows.net", "port": 6379, "password":"mslEyUxY5x9pj9HPL6wRzjglqFhr2KyGNAzCaFYNpw8="}, "custom": "value"})
   })
 })

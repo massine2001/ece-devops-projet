b98ffa171c4f26b79f9f8f7ceeaba27341be4e8c
Agharmiou Massine <agharmioumassine@gmail.com>
2023-11-09 18:15:44 +0100

    fix: correction test redis

--- userapi/test/dbClient.js
+++ userapi/test/dbClient.js
@@ -1,14 +1,21 @@
-const { expect } = require('chai')
+const { expect } = require('chai');
 
-let db
+let db;
 
 describe('Redis', () => {
-  
-  before(() => {
-    db = require('../src/dbClient')
-  })
-  
+  before((done) => {
+    db = require('../src/dbClient');
+    db.on('connect', () => {
+      console.log('Connected to Redis');
+      done();
+    });
+    db.on('error', (err) => {
+      console.error('Error connecting to Redis: ', err);
+      done(err);
+    });
+  });
+
   it('should connect to Redis', () => {
-    expect(db.connected).to.eql(true)
-  })
-})
+    expect(db).to.not.be.undefined;
+  });
+});

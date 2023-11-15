adddf3b6b6072fd166411db8c6599664de4a1a2a
Agharmiou Massine <agharmioumassine@gmail.com>
2023-11-15 18:26:06 +0100

    update: ajout test pour ping redis

--- userapi/test/dbClient.js
+++ userapi/test/dbClient.js
@@ -18,4 +18,24 @@ describe('Redis', () => {
   it('should connect to Redis', () => {
     expect(db).to.not.be.undefined;
   });
+
+  it('should send PING to Redis', async () => {
+    const originalPing = db.ping;
+    db.ping = () => Promise.resolve('PONG'); 
+
+    await expect(db.sendPing()).to.eventually.equal('PONG');
+
+    db.ping = originalPing;
+  });
+
+  it('should send PING at regular intervals', async () => {
+    const originalPing = db.ping;
+    db.ping = () => Promise.resolve('PONG');
+
+    await new Promise(resolve => setTimeout(resolve, 3 * 1000));
+
+    await expect(db.sendPing()).to.eventually.equal('PONG');
+
+    db.ping = originalPing;
+  });
 });

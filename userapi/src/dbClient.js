15b127756bcb7932b2cb93c10ab3be4098f1d734
Agharmiou Massine <agharmioumassine@gmail.com>
2023-11-15 19:38:24 +0100

    fix: bug

--- userapi/src/dbClient.js
+++ userapi/src/dbClient.js
@@ -1,42 +1,18 @@
-const redis = require("redis");
-const configure = require('./configure');
+var redis = require("redis");
+const configure = require('./configure')
 
-const config = configure();
-const db = redis.createClient({
+const config = configure()
+var db = redis.createClient({
   host: process.env.REDIS_HOST || config.redis.host,
   port: process.env.REDIS_PORT || config.redis.port,
   password: process.env.REDIS_PASSWORD || config.redis.password,
   retry_strategy: () => {
-    return new Error("Retry time exhausted");
-  },
-});
-
-const sendPing = async () => {
-  try {
-    const pingResult = await new Promise((resolve, reject) => {
-      db.ping((error, result) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(result);
-        }
-      });
-    });
-    console.log(`PING réussi. Réponse : ${pingResult}`);
-    return pingResult;
-  } catch (error) {
-    console.error(`Erreur lors de l'envoi de PING : ${error.message}`);
-    throw error;
+    return new Error("Retry time exhausted")
   }
-};
+})
 
-const pingInterval = setInterval(sendPing, 3 * 60 * 1000);
-
-// Puisque le cache Redis se ferme après un intervalle de 10 minutes,
-// au lieu d'utiliser db.quit(), on laisse simplement l'intervalle s'écouler.
-process.on('SIGINT', function () {
-  clearInterval(pingInterval);
-  process.exit();
+process.on('SIGINT', function() {
+  db.quit();
 });
 
-module.exports = db;
+module.exports = db
--- userapi/test/dbClient.js
+++ userapi/test/dbClient.js
@@ -1,33 +1,10 @@
-// test/dbClient.js
 const { expect } = require('chai');
-const redis = require("redis");
-const configure = require('../src/configure');
 
-const config = configure();
-const db = redis.createClient({
-  host: process.env.REDIS_HOST || config.redis.host,
-  port: process.env.REDIS_PORT || config.redis.port,
-  password: process.env.REDIS_PASSWORD || config.redis.password,
-  retry_strategy: () => {
-    return new Error("Retry time exhausted");
-  }
-});
-
-const sendPing = async () => {
-  try {
-    const pingResult = await db.ping();
-    console.log(`PING réussi. Réponse : ${pingResult}`);
-    return pingResult;
-  } catch (error) {
-    console.error(`Erreur lors de l'envoi de PING : ${error.message}`);
-    throw error;
-  }
-};
-
-const pingInterval = setInterval(sendPing, 3 * 60 * 1000);
+let db;
 
 describe('Redis', () => {
   before((done) => {
+    db = require('../src/dbClient');
     db.on('connect', () => {
       console.log('Connected to Redis');
       done();
@@ -40,30 +17,5 @@ describe('Redis', () => {
 
   it('should connect to Redis', () => {
     expect(db).to.not.be.undefined;
-  });
-
-  it('should send PING to Redis', async () => {
-    const originalPing = db.ping;
-    db.ping = () => Promise.resolve('PONG');
-
-    await expect(sendPing()).to.eventually.equal('PONG');
-
-    db.ping = originalPing;
-  });
-
-  it('should send PING at regular intervals', async () => {
-    const originalPing = db.ping;
-    db.ping = () => Promise.resolve('PONG');
-
-    await new Promise(resolve => setTimeout(resolve, 3 * 1000));
-
-    await expect(sendPing()).to.eventually.equal('PONG');
-
-    db.ping = originalPing;
-  });
-
-  after(() => {
-    clearInterval(pingInterval);
-    db.quit();
   });
 });

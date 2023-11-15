7579df0f84f5ff90dd874585a27ecbca3a2dfbc9
Agharmiou Massine <agharmioumassine@gmail.com>
2023-11-15 18:53:14 +0100

    fix: exporter la fonction send ping

--- userapi/src/dbClient.js
+++ userapi/src/dbClient.js
@@ -1,19 +1,27 @@
-var redis = require("redis");
-const configure = require('./configure')
+const redis = require("redis");
+const configure = require('./configure');
 
-const config = configure()
-var db = redis.createClient({
+const config = configure();
+const db = redis.createClient({
   host: process.env.REDIS_HOST || config.redis.host,
   port: process.env.REDIS_PORT || config.redis.port,
   password: process.env.REDIS_PASSWORD || config.redis.password,
   retry_strategy: () => {
     return new Error("Retry time exhausted");
-  }
+  },
 });
 
 const sendPing = async () => {
   try {
-    const pingResult = await db.ping();
+    const pingResult = await new Promise((resolve, reject) => {
+      db.ping((error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result);
+        }
+      });
+    });
     console.log(`PING réussi. Réponse : ${pingResult}`);
     return pingResult;
   } catch (error) {

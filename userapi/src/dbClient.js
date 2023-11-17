9544c8cd9f8c139d9c60729ee29d4a77ccd24650
Agharmiou Massine <agharmioumassine@gmail.com>
2023-11-17 09:06:04 +0100

    update: ajout du ping redis

--- userapi/src/dbClient.js
+++ userapi/src/dbClient.js
@@ -1,18 +1,23 @@
 var redis = require("redis");
-const configure = require('./configure')
+const configure = require('./configure');
+const pingService = require('./pingService');
 
-const config = configure()
+const config = configure();
 var db = redis.createClient({
   host: process.env.REDIS_HOST || config.redis.host,
   port: process.env.REDIS_PORT || config.redis.port,
   password: process.env.REDIS_PASSWORD || config.redis.password,
   retry_strategy: () => {
-    return new Error("Retry time exhausted")
+    return new Error("Retry time exhausted");
   }
-})
+});
 
+const pingInterval = setInterval(() => pingService(db), 3 * 60 * 1000);
+
+// Gérer l'arrêt de l'application
 process.on('SIGINT', function() {
+  clearInterval(pingInterval);
   db.quit();
 });
 
-module.exports = db
+module.exports = db;
--- userapi/test/dbClient.js
+++ userapi/test/dbClient.js
@@ -1,4 +1,5 @@
 const { expect } = require('chai');
+const pingService = require('../src/pingService');
 
 let db;
 
@@ -17,5 +18,29 @@ describe('Redis', () => {
 
   it('should connect to Redis', () => {
     expect(db).to.not.be.undefined;
+  });
+
+  it('should send PING to Redis', async () => {
+    const originalPing = db.ping;
+    db.ping = () => Promise.resolve('PONG'); 
+
+    const result = await pingService(db);
+
+    expect(result).to.equal('PONG');
+
+    db.ping = originalPing;
+  });
+
+  it('should send PING at regular intervals', async () => {
+    const originalPing = db.ping;
+    db.ping = () => Promise.resolve('PONG');
+
+    await new Promise(resolve => setTimeout(resolve, 3 * 1000));
+
+    const result = await pingService(db);
+
+    expect(result).to.equal('PONG');
+
+    db.ping = originalPing;
   });
 });

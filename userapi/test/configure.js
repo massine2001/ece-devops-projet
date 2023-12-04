64f70a831a94443d1111101bd357e543e77ff9a7
massine2001 <agharmioumassine@gmail.com>
2023-12-04 15:09:12 +0100

    fix: password redis

--- userapi/test/configure.js
+++ userapi/test/configure.js
@@ -4,11 +4,11 @@ const configure = require('../src/configure')
 describe('Configure', () => {
   it('load default json configuration file', () => {
     const config = configure()
-    expect(config.redis).to.eql({"host": "ece-devops-projet-redis.redis.cache.windows.net", "port": 6379, "password":"w7Mfof6LzHFTZLnLmZTNEMu181YIgdp1aAzCaFsJ1dw="})
+    expect(config.redis).to.eql({"host": "redis-ece.redis.cache.windows.net", "port": 6379, "password":"70RQTfs271089tlLzcCDtRe0t2frOCj2KAzCaLFhWfk="})
   })
   it('load custom configuration', () => {
     const config_custom = {"custom": "value"}
     const config = configure(config_custom)
-    expect(config).to.eql({"redis": {"host": "ece-devops-projet-redis.redis.cache.windows.net", "port": 6379, "password":"w7Mfof6LzHFTZLnLmZTNEMu181YIgdp1aAzCaFsJ1dw="}, "custom": "value"})
+    expect(config).to.eql({"redis": {"host": "redis-ece.redis.cache.windows.net", "port": 6379, "password":"70RQTfs271089tlLzcCDtRe0t2frOCj2KAzCaLFhWfk="}, "custom": "value"})
   })
 })
